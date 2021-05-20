75e9a31a0b1a634fd67d91fb4e6881290da67957
marbielto <marcosgabrielbarreto5@gmail.com>
2021-05-20 00:58:33 -0300

    Trocar de cena
    Trocar de cena através de um trigger

---
+++ TrocarCena.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrocarCena : MonoBehaviour
+{	
+	public int cena;
+    
+    void OnTriggerEnter (Collider hit){
+    	if(hit.tag == "Player"){
+    		SceneManager.LoadScene(cena);
+    	}
+    }
+}
